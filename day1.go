55eb8c9893c3f45b0742d08312507b1abb27d319
Dan <lyingcake77@Gmail.com>
2022-12-12 22:36:54 -0800

    day 1 part 2

--- day1.go
+++ day1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -22,17 +23,22 @@ func main() {
 	readFile.Close()
 	maxAmount := 0
 	currentAmount := 0
+	listOShit := []int{}
 	for _, line := range lines {
 		if line == "" {
 			if currentAmount > maxAmount {
 				maxAmount = currentAmount
 			}
+			listOShit = append(listOShit, currentAmount)
 			currentAmount = 0
+
 		} else {
 			tempResult, _ := strconv.ParseInt(line, 10, 64)
 			currentAmount = currentAmount + int(tempResult)
 		}
 
 	}
+	sort.Ints(listOShit)
 	fmt.Println("max:", maxAmount)
+	fmt.Println("max 3:", listOShit[len(listOShit)-1]+listOShit[len(listOShit)-2]+listOShit[len(listOShit)-3])
 }
